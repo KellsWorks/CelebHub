4849fa559fd1d3cd6c0fb46372914fa6e8ad1f3a
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-12 13:53:19 +0200

    Home
    Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {Text, StyleSheet} from 'react-native';
+import {BottomNavigation} from 'react-native-paper';
+
+const MusicRoute = () => <Text>Music</Text>;
+
+const AlbumsRoute = () => <Text>Albums</Text>;
+
+const RecentsRoute = () => <Text>Recent</Text>;
 
 const Home = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {key: 'music', title: 'Music', icon: 'queue-music'},
+    {key: 'albums', title: 'Albums', icon: 'message-circle'},
+    {key: 'recent', title: 'Recent', icon: 'history'},
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: MusicRoute,
+    albums: AlbumsRoute,
+    recents: RecentsRoute,
+  });
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-    </View>
+    <BottomNavigation
+      navigationState={{index, routes}}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   );
 };
 
--- src/screens/SplashScreen.js
+++
@@ -1,6 +0,0 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
-import config from "../config"
-import { useFonts, Rubik_700Bold, Rubik_400Regular, Rubik_500Medium } from '@expo-google-fonts/rubik';
-import { AppLoading } from 'expo';

3d305b2d3ee84ba97dce5a7158d310806016e24a
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-01-12 10:24:58 +0200

    Fonts
    Fonts

--- src/screens/components/Page.js
+++ src/screens/components/Page.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements';
 
 const Page = ({ backgroundColor, iconName, title }) => {
@@ -16,12 +16,19 @@ const Page = ({ backgroundColor, iconName, title }) => {
     >
       <Icon name={iconName} type="feather" size={172} color="white" />
       <View style={{ marginTop: 16 }}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}>
+        <Text style={ styles.baseText } >
           {title}
         </Text>
       </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'PoppinsRegular'
+    , fontSize: 24, fontWeight: 'bold', color: 'white'
+  }
+});
 
 export default Page;

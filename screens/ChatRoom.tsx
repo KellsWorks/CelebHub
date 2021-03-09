eb89761dc681dd287b22d442a86cadc14cbb4a2c
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-09 18:19:54 +0200

    Chat room

--- screens/ChatRoom.tsx
+++ screens/ChatRoom.tsx
@@ -1,12 +1,39 @@
-import React, { Component } from 'react'
-import { ImageBackground, Text, View } from 'react-native'
+import React, { useState, useCallback, useEffect } from 'react';
+import { ImageBackground, Text, View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const ChatRoom = () => {
 
-    
+    const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+
     return(
         <ImageBackground source={require('../assets/images/BG.png')} style={{ width: '100%', height: '100%' }}>
-            <Text>Chat room</Text>
+            <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: 1,
+            }}
+            />
         </ImageBackground>
     );
 }

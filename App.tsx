2dbe42027acbc191d2b6cd04595e5842f7f0c519
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-03-09 14:35:15 +0200

    Fonts + AppLoading

--- App.tsx
+++ App.tsx
@@ -1,17 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+import AppLoading from 'expo-app-loading';
+import * as Font from 'expo-font';
+
 import { Text, View, StyleSheet } from 'react-native'
 import LottieView from 'lottie-react-native'
 
+const LoadFonts = () =>{
+  return Font.loadAsync({
+    'Font-normal': require('./assets/fonts/poppins_regular.ttf'),
+    'Font-medium': require('./assets/fonts/poppins_medium.ttf'),
+    'Font-bold': require('./assets/fonts/poppins_bold.ttf'),
+  });
+}
+
 export default function App() {
+
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if(!fontLoaded){
+    return(
+      <AppLoading
+        startAsync={LoadFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={(err: any) => console.error(err)}
+      />
+    );
+  }
 
   if (!isLoadingComplete) {
     return (
